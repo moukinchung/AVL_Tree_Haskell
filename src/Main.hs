b06a32b1ca00d6e0a3393a46732213d9a2487584
moukinchung <32200188+moukinchung@users.noreply.github.com>
2018-08-06 21:54:01 -0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,3 @@
-module Main where
-
 import System.IO
 
 data TreeAvl = Vazio | No (TreeAvl) Int (TreeAvl)
@@ -12,7 +10,7 @@ busca x (No l c r) | x == c = True -- Se o elemento for igual ao nó, sucesso na
                    | x >  c = busca x r -- Se elemento for maior ao nó, busca-se no filho a direita
 
 altura :: TreeAvl -> Int
-altura Vazio      = -1 -- Se vazio, altura é -1
+altura Vazio      = 0 -- Se vazio, altura é 0
 altura (No l c r) = 1 + (max (altura l) (altura r)) -- Se for um nó, somar um com a maior altura dos filhos
 
 balanceado :: TreeAvl -> Bool
@@ -32,21 +30,21 @@ direita :: TreeAvl -> TreeAvl
 direita Vazio = Vazio
 direita (No l c r) = r
 
-rotacaoLL :: TreeAvl -> TreeAvl
+rotacaoLL :: TreeAvl -> TreeAvl --Rotacao Simples a direita
 rotacaoLL Vazio = Vazio
 rotacaoLL (No l c r) = No (esquerda l) (valor l) (No (direita l) c r)
 
-rotacaoRR :: TreeAvl -> TreeAvl
+rotacaoRR :: TreeAvl -> TreeAvl --Rotacao Simples a esquerda
 rotacaoRR Vazio = Vazio
 rotacaoRR (No l c r) = No (No l c (esquerda r)) (valor r) (direita r)
 
-rotacaoLR :: TreeAvl -> TreeAvl
+rotacaoLR :: TreeAvl -> TreeAvl --Rotacao esquerda-direita (Dupla a esquerda)
 rotacaoLR Vazio = Vazio
 rotacaoLR (No l c r) = No (No (esquerda l) (valor l) (esquerda (direita l)))
                           (valor (direita l))
                           (No (direita (direita l)) c r)
 
-rotacaoRL :: TreeAvl -> TreeAvl
+rotacaoRL :: TreeAvl -> TreeAvl --Rotacao direita-esquerda (Dupla a direita)
 rotacaoRL Vazio = Vazio
 rotacaoRL (No l c r) = No (No l c (esquerda (esquerda r)))
                           (valor (esquerda r))
@@ -54,7 +52,23 @@ rotacaoRL (No l c r) = No (No l c (esquerda (esquerda r)))
 
 inserir :: Int -> TreeAvl -> TreeAvl
 inserir x Vazio = No Vazio x Vazio
+inserir x (No l c r) | x < c = rebalance(No (inserir x l) c r)
+                     | x > c = rebalance(No l c (inserir x r))
+                     | otherwise = (No l c r)
 
+rebalance :: TreeAvl -> TreeAvl
+rebalance (No l c r) | balanceado (No l c r) == True = (No l c r)
+                     | alturalr == (-2) && altura_r==(-1) = rotacaoRR (No l c r) --Rotacao Simples a esquerda
+                     | alturalr == 2    && altura_l==1    = rotacaoLL (No l c r) --Rotacao Simples a direita
+                     | alturalr == (-2) && altura_r==1    = rotacaoLR (No l c r) --Rotacao esquerda-direita (Dupla a esquerda)
+                     | alturalr == 2    && altura_l==(-1) = rotacaoRL (No l c r) --Rotacao direita-esquerda (Dupla a direita)
+                      where l_l = (esquerda l)
+                            l_r = (direita l)
+                            r_l = (esquerda r)
+                            r_r = (direita r)
+                            alturalr = (altura l)-(altura r)
+                            altura_r = (altura r_l)-(altura r_r)
+                            altura_l = (altura l_l)-(altura l_r)
 
 remover :: Int -> TreeAvl -> TreeAvl
 remover x Vazio              = Vazio

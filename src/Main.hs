23610d4b1b9ca387f8cfbd78d921d7bfcfd3dddb
moukinchung <32200188+moukinchung@users.noreply.github.com>
2018-08-11 19:49:02 -0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -2,62 +2,61 @@ import System.IO
 import Test.QuickCheck
 
 ----Código da Arvore AVL----
-data TreeAvl = Vazio | No (TreeAvl) Int (TreeAvl)
-                 deriving (Show, Eq)
+data TreeAvl a = Vazio | No (TreeAvl a) a (TreeAvl a)
+                 deriving (Show, Eq, Ord)
 
-busca :: Int -> TreeAvl -> Bool
+busca :: (Ord a, Eq a) => a -> TreeAvl a -> Bool
 busca x Vazio = False
-busca x (No l c r) | x == c = True -- Se o elemento for igual ao nó, sucesso na busca
-                   | x <  c = busca x l -- Se elemento for menor ao nó, busca-se no filho a esquerda
-                   | x >  c = busca x r -- Se elemento for maior ao nó, busca-se no filho a direita
+busca x (No l c r) | x == c = True
+                   | x <  c = busca x l
+                   | x >  c = busca x r
 
-valor :: TreeAvl -> Int --Obtem o valor de filho
-valor Vazio = 0
+valor :: (Ord a, Eq a) => TreeAvl a -> a --Obtem o valor de filho
 valor (No l c r) = c
 
-esquerda :: TreeAvl -> TreeAvl --Obtem o nó a esquerda
+esquerda :: (Ord a, Eq a) => TreeAvl a-> TreeAvl a--Obtem o nó a esquerda
 esquerda Vazio = Vazio
 esquerda (No l c r) = l
 
-direita :: TreeAvl -> TreeAvl --Obtem o nó a direita
+direita :: (Ord a, Eq a) => TreeAvl a-> TreeAvl a--Obtem o nó a direita
 direita Vazio = Vazio
 direita (No l c r) = r
 
-rotacaoLL :: TreeAvl -> TreeAvl --Rotacao Simples a direita
+rotacaoLL :: (Ord a, Eq a) => TreeAvl a -> TreeAvl a--Rotacao Simples a direita
 rotacaoLL Vazio = Vazio
 rotacaoLL (No Vazio c Vazio) = (No Vazio c Vazio)
 rotacaoLL (No l c r) = No (esquerda l) (valor l) (No (direita l) c r)
 
-rotacaoRR :: TreeAvl -> TreeAvl --Rotacao Simples a esquerda
+rotacaoRR :: (Ord a, Eq a) => TreeAvl a -> TreeAvl a--Rotacao Simples a esquerda
 rotacaoRR Vazio = Vazio
 rotacaoRR (No Vazio c Vazio) = (No Vazio c Vazio)
 rotacaoRR (No l c r) = No (No l c (esquerda r)) (valor r) (direita r)
 
-rotacaoLR :: TreeAvl -> TreeAvl --Rotacao Dupla a direita
+rotacaoLR :: (Ord a, Eq a) => TreeAvl a -> TreeAvl a--Rotacao Dupla a direita
 rotacaoLR Vazio = Vazio
 rotacaoLR (No Vazio c Vazio) = (No Vazio c Vazio)
 rotacaoLR (No l c r) = No (No (esquerda l) (valor l) (esquerda (direita l)))
                           (valor (direita l))
                           (No (direita (direita l)) c r)
 
-rotacaoRL :: TreeAvl -> TreeAvl --Rotacao Dupla a esquerda
+rotacaoRL :: (Ord a, Eq a) => TreeAvl a -> TreeAvl a--Rotacao Dupla a esquerda
 rotacaoRL Vazio = Vazio
 rotacaoRL (No Vazio c Vazio) = (No Vazio c Vazio)
 rotacaoRL (No l c r) = No (No l c (esquerda (esquerda r)))
                           (valor (esquerda r))
                           (No (direita (esquerda r)) (valor r) (direita r))
 
-inserir :: Int -> TreeAvl -> TreeAvl
+inserir :: (Ord a, Eq a) => a -> TreeAvl a-> TreeAvl a
 inserir x Vazio = No Vazio x Vazio
 inserir x (No l c r) | x < c = rebalance(No (inserir x l) c r)
                      | x > c = rebalance(No l c (inserir x r))
                      | otherwise = (No l c r)
 
-altura :: TreeAvl -> Int
+altura :: (Ord a, Eq a) => TreeAvl a-> Int
 altura Vazio      = 0 -- Se vazio, altura é 0
 altura (No l c r) = 1 + (max (altura l) (altura r)) -- Se for um nó, somar um com a maior altura dos filhos
 
-rebalance :: TreeAvl -> TreeAvl
+rebalance :: (Ord a, Eq a) => TreeAvl a -> TreeAvl a
 rebalance (No l c r) | alturalr == (-2) && altura_r== (-1) = rotacaoRR (No l c r) --Rotacao Simples a esquerda
                      | alturalr == 2    && altura_l== 1    = rotacaoLL (No l c r) --Rotacao Simples a direita
                      | alturalr == (-2) && altura_r== 1    = rotacaoRL (No l c r) --Rotacao esquerda-direita (Dupla a esquerda)---
@@ -71,7 +70,7 @@ rebalance (No l c r) | alturalr == (-2) && altura_r== (-1) = rotacaoRR (No l c r
                             altura_r = (altura r_l)-(altura r_r)
                             altura_l = (altura l_l)-(altura l_r)
 
-remover :: Int -> TreeAvl -> TreeAvl
+remover :: (Ord a, Eq a) => a -> TreeAvl a -> TreeAvl a
 remover x Vazio              = Vazio
 remover x (No Vazio c Vazio) = if x == c then Vazio else (No Vazio c Vazio)
 
@@ -90,20 +89,22 @@ remover x (No l c r)         | x == c = rebalance(No l c' r')
                                where c' = menorValor r
                                      r' = remover c' r
 
-menorValor :: TreeAvl -> Int
+menorValor :: (Ord a, Eq a) => TreeAvl a -> a
 menorValor avl = head(geraLista avl)
 
-geraLista :: TreeAvl -> [Int]
+geraLista :: (Ord a, Eq a) => TreeAvl a -> [a]
 geraLista Vazio = []
 geraLista (No l c r) = geraLista l ++ [c] ++ geraLista r
 
-----Manipulação da Arvore AVL----
+----Testes da Arvore AVL----
 
-geraAVL :: TreeAvl -> [Int] -> TreeAvl -- Gera uma Arvore AVL a partir de uma lista
+geraAVL :: (Ord a, Eq a) => TreeAvl a -> [a] -> TreeAvl a-- Gera uma Arvore AVL a partir de uma lista
 geraAVL avl [] = avl
 geraAVL avl (x:xs) = inserir x (geraAVL avl xs)
 
 
-removerAVL :: TreeAvl -> [Int] -> TreeAvl -- Remove valores de uma AVL a partir deu uma lista
+removerAVL :: (Ord a, Eq a) => TreeAvl a -> [a] -> TreeAvl a-- Remove valores de uma AVL a partir de uma lista
 removerAVL avl [] = avl
 removerAVL avl (x:xs) = remover x (removerAVL avl xs)
+
+----------------------------------
